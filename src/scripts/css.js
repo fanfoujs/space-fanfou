48c4744769d218d5487b0e3be0f7e161d9d41ebc
upsuper <quanxunzhen@gmail.com>
2011-05-15 19:34:13 +0800

    improve styles injecting

--- src/scripts/css.js
+++ src/scripts/css.js
@@ -1,28 +1,10 @@
 (function() {
-	var css_switches = [
-		['font_reset', 'font-reset-all.css', 'font-reset-en.css'],
-		['translucent_sidebar', 'translucent-sidebar.css', null]
-	];
-
-	var keys = [];
-	for (var i = 0; i < css_switches.length; ++i)
-		keys.push(css_switches[i][0]);
 	chrome.extension.sendRequest(
-		{
-			func: 'readData',
-			data: keys
-		},
+		{ func: 'getStyles' },
 		function(data) {
-			for (var i = 0; i < css_switches.length; ++i) {
-				var css_file = data[i] ?
-					css_switches[i][1] : css_switches[i][2];
-				if (! css_file) continue;
-				var $style = $('<link>');
-				$style.attr('rel', 'stylesheet');
-				$style.attr('href',
-					chrome.extension.getURL('styles/' + css_file));
-				$('head').append($style);
-			}
+            var $style = document.createElement('style');
+            $style.appendChild(document.createTextNode(data));
+            document.documentElement.appendChild($style);
 		}
 	);
 })();
