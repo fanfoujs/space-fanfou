dda50d5f8ffe2702f30111753761bfeb415f3ea0
upsuper <quanxunzhen@gmail.com>
2011-06-06 17:19:59 +0800

    modify probe

--- src/common/probe.js
+++ src/common/probe.js
@@ -1,13 +1,17 @@
 (function() {
-    var interval = setInterval(function() {
-        if (jQuery && FF) {
-            clearInterval(interval);
-            var $ = jQuery;
-            var $meta = $('<meta>');
-            $meta.attr('name', 'spacefanfou-flags');
-            $meta.attr('content', 'libs_ok');
-            $meta.attr('id', 'sf_flag_libs_ok');
-            $('head').append($meta);
-        }
-    }, 200);
+    function setWaiting() {
+        setTimeout(function() {
+            if (! window.jQuery || ! window.FF) {
+                setWaiting();
+            } else {
+                var $ = jQuery;
+                var $meta = $('<meta>');
+                $meta.attr('name', 'spacefanfou-flags');
+                $meta.attr('content', 'libs_ok');
+                $meta.attr('id', 'sf_flag_libs_ok');
+                $('head').append($meta);
+            }
+        }, 200);
+    }
+    setWaiting();
 })();
