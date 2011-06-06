6a90b66497c89e6d51a882806af661236e8270b8
upsuper <quanxunzhen@gmail.com>
2011-06-06 17:17:18 +0800

    modify image-uploading to fit new framework

--- src/plugins/image_uploading.js
+++ src/plugins/image_uploading.js
@@ -1,42 +1,45 @@
-SF.checkAndExec('image_uploading', [], function() {
-	var $message = $('#message');
-	if (! $message.size()) return;
-	var $h2 = $('h2', $message);
-	var $textarea = $('textarea', $message);
-	var $action = $('input[name=action]', $message);
-	var $button = $('<a>');
-	$button.attr('href', 'javascript:;');
-	$button.attr('id', 'u_button');
-	var $upload = $('<input>');
-	$upload.attr('type', 'file');
-	$upload.attr('name', 'picture');
-	$upload.attr('size', '1');
-	$button.append($upload);
-	$h2.append($button);
-	var $file = $('<span>');
-	$file.attr('id', 'u_filename');
-	$h2.append($file);
-	$upload.change(function() {
-		var $message = $('#message');
-		var $textarea = $('textarea', $message);
-		$file.text('');
-		$file.text($upload.value);
-		var $iframe = $('<iframe>');
-		$iframe.attr('name', 'upload_image');
-		$message.append($iframe);
-		$photo = $('<input>');
-		$photo.attr('type', 'hidden');
-		$photo.attr('name', 'photo');
-		$message.append($photo);
-		$textarea.attr('name', 'desc');
-		$action.val('photo.upload');
-		$message.attr('enctype', 'multipart/form-data');
-		$message.attr('action', 'http://m.fanfou.com/home');
-		$message.attr('target', 'upload_image');
-		$message.submit(function() {
-			$iframe.load(function() {
-				window.location.reload();
-			});
-		});
-	});
-});
+SF.pl.image_uploading = (function($) {
+    var $message = $('#message');
+    if (! $message.length) return SF.empty_pl;
+
+    var $h2 = $('h2', $message);
+    var $textarea = $('textarea', $message);
+    var $action = $('input[name=action]', $message);
+    var $button = $('<a>');
+    $button.attr('href', 'javascript:;');
+    $button.attr('id', 'u_button');
+    var $upload = $('<input>');
+    $upload.attr('type', 'file');
+    $upload.attr('name', 'picture');
+    $upload.attr('size', '1');
+    $button.append($upload);
+
+    $upload.change(function() {
+        var $iframe = $('<iframe>');
+        $iframe.attr('name', 'upload_image');
+        $message.append($iframe);
+        $photo = $('<input>');
+        $photo.attr('type', 'hidden');
+        $photo.attr('name', 'photo');
+        $message.append($photo);
+        $textarea.attr('name', 'desc');
+        $action.val('photo.upload');
+        $message.attr('enctype', 'multipart/form-data');
+        $message.attr('action', 'http://m.fanfou.com/home');
+        $message.attr('target', 'upload_image');
+        $message.submit(function() {
+            $iframe.load(function() {
+                window.location.reload();
+            });
+        });
+    });
+
+    return {
+        load: function() {
+            $h2.append($button);
+        },
+        unload: function() {
+            $button.detach();
+        }
+    };
+})(jQuery);
