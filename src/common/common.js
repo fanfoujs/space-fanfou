4a3fa1b9bcf0eb44425ace906a6b179bd64020c6
upsuper <quanxunzhen@gmail.com>
2011-06-09 20:55:31 +0800

    minor modify of common script

--- src/common/common.js
+++ src/common/common.js
@@ -3,6 +3,7 @@ var SF = (function() {
         pl: { },
         plugin: function(func) {
             var empty_func = function() { };
+            if (! func) func = { };
             this.update = func.update ? func.update : empty_func;
             this.load = func.load ? func.load : empty_func;
             this.unload = func.unload ? func.unload : empty_func;
