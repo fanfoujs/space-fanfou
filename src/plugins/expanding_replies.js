7b4c080c203da003c3810b7128d9f1a860037af2
upsuper <quanxunzhen@gmail.com>
2011-06-21 21:33:28 +0800

    fix some bugs of expanding-replies

--- src/plugins/expanding_replies.js
+++ src/plugins/expanding_replies.js
@@ -1,76 +1,96 @@
 SF.pl.expanding_replies = new SF.plugin((function($) {
-	var $stream = $('#stream');
-	if (! $stream.length) return;
+    var $stream = $('#stream');
+    if (! $stream.length) return;
 
     var replies_number;
-
+    
     function showWaiting($e) {
         var $wait = $('<li>');
         $wait.addClass('reply waiting');
+        var id = 'waiting_' + Math.random().toString().slice(2);
+        $wait.attr('id', id);
         $e.replaceWith($wait);
-        return $wait;
+        return id;
     }
 
-    function displayReplyList(url, $before, num) {
+    function displayReplyList(url, before, num, first) {
         if (num == 0) {
             var $more = $('<li>');
             $more.attr('href', url);
             $more.addClass('reply more');
             $more.text('继续展开');
+            var $before = $('#' + before);
             $more.insertBefore($before);
             $before.remove();
             return;
         }
         $.get(url, function(data) {
+            var $before = $('#' + before);
             var avatar = /<div id="avatar">(.+?)<\/div>/.exec(data)[1];
             var author_exp = /<h1>(.+?)<\/h1>/g;
             author_exp.lastIndex = data.indexOf('<div id="latest">');
             var author = author_exp.exec(data)[1];
             var content = /<h2>([\s\S]+?)<\/h2>/.exec(data);
+            var avail = false;
             if (! content) {
-                content = '<strong>此消息已删除</strong>';
+                content = '原消息已删除';
                 spans = '';
             } else {
                 content = content[1];
                 var stamp_pos = content.indexOf('<span class="stamp">');
                 var spans;
                 if (stamp_pos == -1) {
-                    content = '<strong>此用户设置了隐私保护</strong>';
+                    content = '原消息不公开';
                     spans = '';
                 } else {
                     spans = content.substring(stamp_pos);
                     content = content.substring(0, stamp_pos);
                     spans = spans.replace('redirect="/home" ', '');
+                    avail = true;
                 }
             }
             var $li = $('<li>');
-            $li.attr('expended', 'expended');
-            $li.addClass('reply unlight');
-            $li.html(avatar + author +
-                '<span class="content">' + content + '</span>' + spans);
-            FF.app.Stream.attach($li[0]);
-            if (content.indexOf('<img '))
-                FF.app.Zoom.init($li[0]);
-            var $links = $('a', $li);
-            $links.eq(0).addClass('avatar');
-            $links.eq(1).addClass('author');
-            var $stamp = $('.stamp', $li);
-            if (! $stamp.length) {
-                url = '';
-            } else {
-                var $reply = $('.reply', $stamp);
-                if ($reply.length == 0) {
+            if (avail) {
+                $li.attr('expended', 'expended');
+                $li.addClass('reply unlight');
+                $li.html(avatar + author +
+                    '<span class="content">' + content + '</span>' + spans);
+                FF.app.Stream.attach($li[0]);
+                if (content.indexOf('<img '))
+                    FF.app.Zoom.init($li[0]);
+                var $links = $('a', $li);
+                $links.eq(0).addClass('avatar');
+                $links.eq(1).addClass('author');
+                var $stamp = $('.stamp', $li);
+                if (! $stamp.length) {
                     url = '';
                 } else {
-                    url = $('a', $reply).attr('href');
+                    var $reply = $('.reply', $stamp);
+                    if ($reply.length == 0) {
+                        url = '';
+                    } else {
+                        url = $('a', $reply).attr('href');
+                    }
                 }
-            }
-            $li.insertBefore($before);
-            if (! url) {
-                $li.addClass('last');
-                $before.remove();
+                if (first) {
+                    var $hide_replies = $('<li>');
+                    $hide_replies.addClass('reply hide');
+                    $hide_replies.attr('expended', 'expended');
+                    $hide_replies.text('隐藏回复原文');
+                    $hide_replies.insertBefore($before);
+                }
+                $li.insertBefore($before);
+                if (! url) {
+                    $li.addClass('last');
+                    $before.remove();
+                } else {
+                    displayReplyList(url, before, num - 1);
+                }
             } else {
-                displayReplyList(url, $before, num - 1);
+                $li.addClass('reply notavail');
+                $li.attr('href', url);
+                $li.text(content);
+                $before.replaceWith($li);
             }
         });
     }
@@ -104,18 +124,12 @@ SF.pl.expanding_replies = new SF.plugin((function($) {
             $item.click(hideReplyList);
         } else if (! $item.attr('href')) {
             showExpand($item);
-        } else {
+        } else if (! $item.hasClass('notavail')) {
             $item.click(function() {
                 var $t = $(this);
-                var $before = showWaiting($t);
-                if ($t.hasClass('first')) {
-                    var $hide_replies = $('<li>');
-                    $hide_replies.addClass('reply hide');
-                    $hide_replies.attr('expended', 'expended');
-                    $hide_replies.text('隐藏回复原文');
-                    $hide_replies.insertBefore($before);
-                }
-                displayReplyList($item.attr('href'), $before, replies_number);
+                displayReplyList($item.attr('href'),
+                    showWaiting($t), replies_number,
+                    $t.hasClass('first'));
             });
         }
     }
@@ -162,7 +176,7 @@ SF.pl.expanding_replies = new SF.plugin((function($) {
             if (! $ol.length) return;
             $ol.unbind('DOMNodeInserted', onDOMNodeInserted);
             $ol.unbind('DOMNodeRemoved', onDOMNodeRemoved);
-            $('li.reply', $ol).fadeOut();
+            $('li.reply', $ol).remove();
             $('li[expended]', $ol).removeAttr('expended');
         }
     };
