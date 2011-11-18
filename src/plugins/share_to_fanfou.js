7bd39162bf90804f0e5846f8f5ea65600bc89822
Kevin <dksnow2002@gmail.com>
2011-11-18 15:24:23 +0800

    revert previuos share-to-fanfou

--- src/plugins/share_to_fanfou.js
+++ src/plugins/share_to_fanfou.js
@@ -1,9 +1,18 @@
 SF.pl.share_to_fanfou = new SF.plugin((function() {
     var menu_share = null;
-    var share_script = "var d=document,w=window,l=d.location,e=encodeURIComponent,p='?u='+e(l.href)+'&t='+e(d.title)+'&d='+e(w.getSelection?w.getSelection().toString():d.getSelection?d.getSelection():d.selection.createRange().text)+'&s=bm';w.open('http://fanfou.com/sharer'+p,'sharer','toolbar=0,status=0,resizable=0,width=600,height=400');void(0)";
+    //var share_script = "var d=document,w=window,l=d.location,e=encodeURIComponent,p='?u='+e(l.href)+'&t='+e(d.title)+'&d='+e(w.getSelection?w.getSelection().toString():d.getSelection?d.getSelection():d.selection.createRange().text)+'&s=bm';w.open('http://fanfou.com/sharer'+p,'sharer','toolbar=0,status=0,resizable=0,width=600,height=400');void(0)";
 
     function onClick(info, tab) {
-        chrome.tabs.executeScript(tab.id, { code: share_script });
+        var select = info.selectionText;
+        var encode = encodeURIComponent;
+        window.open('http://fanfou.com/sharer?' +
+                    'u=' + encode(tab.url) +
+                    '&t=' + encode(tab.title) +
+                    '&s=bm' +
+                    '&d=' + encode(select ? select : ''),
+                    'sharer',
+                    'toolbar=0,status=0,resizable=0,width=600,height=400');
+        //chrome.tabs.executeScript(tab.id, { code: share_script });
     }
 
     return {
