c9473778a101ce1b5f080e3e23d5c55f060e2774
Lacc Riophae <iruif.cn@gmail.com>
2013-11-04 20:03:18 +0800

    fix logo remove beta plugin

--- src/plugins/logo_remove_beta.js
+++ src/plugins/logo_remove_beta.js
@@ -3,7 +3,7 @@ SF.pl.logo_remove_beta = new SF.plugin((function($) {
 	if (! $header.length) return;
 	
 	var attr = 'data-original-logo';
-	var beta = 'url(http://static2.fanfou.com/img/fanfou_beta.png)';
+	var beta_re = /url\("?http:\/\/static\d?\.fanfou\.com\/img\/fanfou_beta\.png"?\)/;
 
 	$header.attr(attr, true);
 	var origin = $header.css('background-image');
@@ -12,7 +12,7 @@ SF.pl.logo_remove_beta = new SF.plugin((function($) {
 	return {
 		load: function() {
 			$('#sf_style_logo_remove_beta').remove();
-			if (origin == beta) {
+			if (beta_re.test(origin)) {
 				$header.css('background-image', 'url(http://static2.fanfou.com/img/fanfou.png)');
 			}
 		},
