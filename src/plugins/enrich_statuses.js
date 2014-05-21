6a20f32a6c88b51a598a9146378b7eaa9e8ee2f8
Lacc Riophae <iruif.cn@gmail.com>
2014-05-21 18:00:34 +0800

    fixed enrich statuses plugin

--- src/plugins/enrich_statuses.js
+++ src/plugins/enrich_statuses.js
@@ -100,7 +100,7 @@ SF.pl.enrich_statuses = new SF.plugin((function($) {
 		var short_url_services = SF.fn.getData('short_url_services');
 		if (short_url_services) {
 			// 识别更多短链接
-			short_url_services['[a-z0-9]{1,5}\.[a-z]{2,3}'] = true;
+			short_url_services['[a-z0-9]{1,5}\\.[a-z]{2,3}'] = true;
 			var re = '^https?:\\/\\/';
 			re += '(?:' + Object.keys(short_url_services).join('|') + ')';
 			re += '\\/\\S+';
@@ -125,14 +125,24 @@ SF.pl.enrich_statuses = new SF.plugin((function($) {
 	}
 
 	var cached_short_urls = SF.fn.getData('cached_short_urls') || { };
-	function expandUrl(url, callback) {
+	function expandUrl(url, callback, original_url) {
+		original_url = original_url || url;
 		if (cached_short_urls[url]) {
 			setTimeout(function() {
 				callback(cached_short_urls[url]);
 			});
 		} else {
 			function cb(long_url) {
-				cached_short_urls[url] = long_url;
+				if (short_url_re.test(long_url) && url != long_url) {
+					return expandeUrl(long_url, callback, original_url);
+				}
+				if (long_url.indexOf('http') !== 0) {
+					setTimeout(function() {
+						expandUrl(url, callback, original_url);
+					}, 5000);
+					return;
+				}
+				cached_short_urls[original_url] = long_url;
 				SF.fn.setData('cached_short_urls', cached_short_urls);
 				callback(long_url);
 			}
@@ -197,6 +207,7 @@ SF.pl.enrich_statuses = new SF.plugin((function($) {
 				! self.longUrl) {
 				setTimeout(function() {
 					self.longUrl = self.url;
+					markAsIgnored();
 				});
 			}
 
