f69e161e2ba23f01b99437bb832faa145cc64dc6
LitoMore <litomore@gmail.com>
2019-04-17 20:36:18 +0800

    修正代码风格

--- src/libs/getExtensionVersion.js
+++ src/libs/getExtensionVersion.js
@@ -1,6 +1,6 @@
 export default () => {
-  const {version, version_name} = chrome.runtime.getManifest()
+  const { version, version_name: name } = chrome.runtime.getManifest()
 
   // version_name 字段用于存放 beta 版本号，为空时表示正式版
-  return version_name || version
+  return name || version
 }
