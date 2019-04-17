bb588b3c8d565be2d5923824d4247bba34a25a1a
LitoMore <LitoMore@users.noreply.github.com>
2019-04-17 19:12:44 +0800

    增加 version_name 支持 (#50)

--- src/libs/getExtensionVersion.js
+++ src/libs/getExtensionVersion.js
@@ -1,1 +1,6 @@
-export default () => chrome.runtime.getManifest().version
+export default () => {
+  const {version, version_name} = chrome.runtime.getManifest()
+
+  // version_name 字段用于存放 beta 版本号，为空时表示正式版
+  return version_name || version
+}
