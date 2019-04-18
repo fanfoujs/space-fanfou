00d6618598f16442cbd0db7fee9e9ba976bc8f2e
Riophae Lee <riophaelee@gmail.com>
2019-04-18 17:10:49 +0800

    修正有时不能去除 logo 的 beta 字样的问题

--- src/features/remove-logo-beta/script(content).js
+++ src/features/remove-logo-beta/script(content).js
@@ -1,3 +1,5 @@
+import keepRetry from '@libs/keepRetry'
+
 const CLASSNAME_FEATURE = 'sf-remove-logo-beta'
 const RE_BETA_LOGO = /url\("?https?:\/\/static\d?\.fanfou\.com\/img\/fanfou_beta\.(png|svg)"?\)/
 
@@ -8,13 +10,23 @@ export default context => {
     header: '.global-header-content',
   })
 
+  function getLogoCssBackgroundImage() {
+    const { header } = elementCollection.getAll()
+    const { backgroundImage } = getComputedStyle(header)
+
+    return backgroundImage
+  }
+
   return {
     applyWhen: () => elementCollection.ready('header'),
 
+    waitReady: () => keepRetry({
+      checker: () => getLogoCssBackgroundImage() !== 'none',
+      delay: 0, // 减少等待以尽早完成替换，避免出现闪烁
+    }),
+
     onLoad() {
-      const { header } = elementCollection.getAll()
-      const originalCssBgImg = getComputedStyle(header).backgroundImage
-      const isBetaLogo = RE_BETA_LOGO.test(originalCssBgImg)
+      const isBetaLogo = RE_BETA_LOGO.test(getLogoCssBackgroundImage())
 
       // 若遇节日 logo，则不予替换
       if (isBetaLogo) document.body.classList.add(CLASSNAME_FEATURE)
