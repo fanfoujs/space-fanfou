1b604423d90e3cb00b3152121d3443ad3b0e3ca0
Fangzhou Li <riophaelee@gmail.com>
2019-05-05 18:07:41 +0800

    改进打包 zip 脚本 (#69)

--- build/pack.js
+++ build/pack.js
@@ -1,17 +1,32 @@
+/* eslint no-console: 0 */
+
 const path = require('path')
 const cp = require('child_process')
 const chalk = require('chalk')
+const timestamp = require('tinydate')('{YYYY}/{MM}/{DD} {HH}:{mm}:{ss}')
 const manifest = require('../static/manifest')
 
-function zip() {
+function log(message) {
+  console.log(`[${chalk.cyan(timestamp())}] ${message}`)
+}
+
+function pack() {
   const version = manifest.version_name || manifest.version
   const zipName = `space-fanfou-${version}.zip`
-  const outPath = path.join(__dirname, '..', zipName)
-  const distPath = path.join(__dirname, '..', 'dist')
-  const command = `rm -f ${outPath} && cd ${distPath} && zip -r ${outPath} ./*`
+  const pkgRootPath = path.join(__dirname, '..')
+  const outPath = path.join(pkgRootPath, zipName)
+  const distPath = path.join(pkgRootPath, 'dist')
+  const command = `rm -f ${JSON.stringify(outPath)} && cd ${JSON.stringify(distPath)} && zip -r ${JSON.stringify(outPath)} *`
 
-  cp.execSync(command)
-  // eslint-disable-next-line no-console
-  console.log(`创建成功：${chalk.green(zipName)}`)
+  log(`正在创建：${chalk.green(zipName)}`)
+
+  cp.exec(command, error => {
+    if (error) {
+      log(chalk.red('创建失败'))
+      console.log(error)
+    } else {
+      log(chalk.green('创建成功'))
+    }
+  })
 }
-zip()
+pack()
