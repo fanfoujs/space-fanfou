b84d4cf2d478f32cc0d302cd763d0538f14420ac
Riophae Lee <riophaelee@gmail.com>
2019-06-20 23:59:16 +0800

    更新依赖

--- src/libs/safeElementReady.js
+++ src/libs/safeElementReady.js
@@ -1,13 +1,4 @@
-import domLoaded from 'dom-loaded'
 import elementReady from 'element-ready'
 
 // 持续查找匹配给定选择器的元素，直到 DOM 加载完成
-// 复制自：https://github.com/sindresorhus/refined-github/blob/7c75af58f77c705bd03d1ec9b5e15b9042884f15/source/libs/utils.js#L67-L78
-export default selector => {
-  const waiting = elementReady(selector)
-
-  domLoaded.then(() => requestAnimationFrame(::waiting.cancel))
-
-  // 若没有匹配到，返回 null
-  return waiting.catch(() => null)
-}
+export default selector => elementReady(selector, { stopOnDomReady: true })
