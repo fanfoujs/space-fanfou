f64ad29c69f65cc9b27ca29cd26f105f9fc8164b
Fangzhou Li <riophaelee@gmail.com>
2019-09-12 12:07:55 +0800

    1.0.1

--- src/features/notifications/update-details@background.js
+++ src/features/notifications/update-details@background.js
@@ -6,10 +6,22 @@ import {
 } from '@constants'
 
 const NOTIFICATION_TIMEOUT = 15 * 1000
-
 export default context => {
   const { readOptionValue, requireModules } = context
   const { storage, notification } = requireModules([ 'storage', 'notification' ])
+
+  // 1.0.0 是一个不能使用的版本，下面的代码使得当用户升级到 1.0.1 后也能看到 1.0.0 的更新通知
+  const patchedVersionHistory = do {
+    const patched = [ ...versionHistory ]
+    const updateDetailsFor100 = versionHistory.find(({ version }) => version === '1.0.0')
+    const updateDetailsFor101 = { ...updateDetailsFor100 }
+    const indexOf100 = patched.indexOf(updateDetailsFor100)
+
+    updateDetailsFor101.version = '1.0.1'
+    patched.splice(indexOf100, 0, updateDetailsFor101)
+
+    patched // eslint-disable-line no-unused-expressions
+  }
 
   function isExtensionUpgraded() {
     return storage.read(
@@ -20,7 +32,7 @@ export default context => {
 
   function getCurrentVersionUpdateDetails() {
     const currentVersion = getExtensionVersion()
-    const versionItem = versionHistory.find(({ version }) => version === currentVersion)
+    const versionItem = patchedVersionHistory.find(({ version }) => version === currentVersion)
 
     if (!versionItem) return ''
     if (versionItem.summary) return versionItem.summary
@@ -42,7 +54,7 @@ export default context => {
     const onClick = () => {
       const url = chrome.runtime.getURL('settings.html') + '#version-history'
 
-      chrome.tabs.create(url)
+      chrome.tabs.create({ url, active: true })
     }
 
     notification.create({
